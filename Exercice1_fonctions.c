28fcfb7db248f6cb37aa566c69f5dff09a3cb4f0
TOMitom910 <73221007+TOMitom910@users.noreply.github.com>
2021-05-28 18:02:50 +0200

    ok

--- Exercice1_fonctions.c
+++ Exercice1_fonctions.c
@@ -5,44 +5,34 @@
 //Exercice 1.1 :
 
 int val;
-
-void f1()
-{
-    printf("bonjour\n");
-    return;
-}
-
-void f2()
-{
-    int i;
-    for(i=0;i<val;i++)
-    {
-        f1();
-    }
-    return;
-}
-
-void f3(val)
-{
-    int i;
-    for(i=0;i<val;i++)
-    {
-        f1();
-    }
-    return(val);
-}
+//prototype
+void f1();
 
 int main()      // exo 1.1
 {
     f1();
-    scanf("%d",&val);
+    /*scanf("%d",&val);
     f2();
-    f3(val);
+    f3(val);*/
+    return 0;
 }
+
+void f1()
+{
+    printf("bonjour\n");
+}
+
+
+
+
+
+
+
+
 
 // Exercice 1.2 :
 
-int n=5;
+/*int n=5;
 
 int main()      // exo 1.2
 {
@@ -77,7 +67,7 @@ int main()
         x++;
     }
     while(x < 1000000000);
-}
+}*/
 
 
 
